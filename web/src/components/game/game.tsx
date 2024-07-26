a0dbd91dd54d79748444dbdd8a975c33a7af1559
Austin Abhari <aabmxer@gmail.com>
2024-07-26 11:27:13 -0600

    DURS A BUG IN MY BOOT

--- web/src/components/game/game.tsx
+++ web/src/components/game/game.tsx
@@ -1,115 +1,131 @@
-import { useEffect, useState } from 'react';
-import { StyledContainer, StyledGrid } from './styled-game';
-import Square from '../square';
-import { checkWinner } from '../../util/check-winner';
-import { useGameContext } from '../../context/GameProvider';
-import styled from 'styled-components';
+import { useEffect, useState } from "react";
+import { StyledContainer, StyledGrid } from "./styled-game";
+import Square from "../square";
+import { checkWinner } from "../../util/check-winner";
+import { useGameContext } from "../../context/GameProvider";
+import styled from "styled-components";
 
 const GamesContainer = styled.div`
-    width: 250px;
-    background: black;
-    height: 100vh;
-    h1 {
-        color: white;
-    }
-`
+	width: 250px;
+	background: black;
+	height: 100vh;
+	h1 {
+		color: white;
+	}
+`;
 
 const TicTacToeContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    flex: 1;
-`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	flex: 1;
+`;
 
 const SaveGameForm = styled.form`
-    display: flex;
-`
+	display: flex;
+`;
 
 const Game = () => {
-    const { games, saveGame } = useGameContext();;
-    const [showSavedGame, setShowSavedGame] = useState('');
-    const [gameName, setGameName] = useState('');
-    const [xTurn, setXTurn] = useState<boolean>(true)
-    const [winner, setWinner] = useState<string | null>(null)
-    const [background, setBackground] = useState<string>('hotpink')
-    const [grid, setGrid] = useState<board>([
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ])
+	const { games, saveGame } = useGameContext();
+	const [showSavedGame, setShowSavedGame] = useState("");
+	const [gameName, setGameName] = useState("");
+	const [xTurn, setXTurn] = useState<boolean>(true);
+	const [winner, setWinner] = useState<string | null>(null);
+	const [background, setBackground] = useState<string>("hotpink");
+	const [grid, setGrid] = useState<board>([
+		["", "", ""],
+		["", "", ""],
+		["", "", ""],
+	]);
 
-    const handleMarker = (index: any) => {
-        const tempGrid = [...grid];
-        index = index.split('-')
-        grid[index[0]][index[1]] = xTurn ? 'x' : 'o'
-        setXTurn(!xTurn)
-        setGrid(tempGrid)
-        setWinner(checkWinner(tempGrid))
-    }
+	const handleMarker = (index: any) => {
+		const tempGrid = [...grid];
+		index = index.split("-");
+		grid[index[0]][index[1]] = xTurn ? "x" : "o";
+		setXTurn(!xTurn);
+		setGrid(tempGrid);
+		setWinner(checkWinner(tempGrid));
+	};
 
-    const handleShowSavedGame = async (gameName: string) => {
-        setShowSavedGame(gameName);
-        const newGame = games.find(c => c.gameName === gameName);
-        if (newGame)
-            setGrid(newGame.board)
-    }
+	const handleShowSavedGame = async (name: string) => {
+		console.log(name);
+		// setShowSavedGame(name);
+		const newGame = games.find((c) => c.name === name);
+		console.log(newGame);
+		if (newGame)
+			setGrid(
+				newGame.board || [
+					["", "", ""],
+					["", "", ""],
+					["", "", ""],
+				]
+			);
+	};
 
-    useEffect(() => {
-        if (winner === null) return;
+	useEffect(() => {
+		if (winner === null) return;
 
-        // const colorInterval = setInterval(() => {
-        //     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        //     setBackground(randomColor);
-        // }, 300)
+		// const colorInterval = setInterval(() => {
+		//     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+		//     setBackground(randomColor);
+		// }, 300)
 
-        // return () => clearInterval(colorInterval);
+		// return () => clearInterval(colorInterval);
+	}, [winner]);
 
-    }, [winner])
+	const handleSaveEvent = (e: any) => {
+		e.preventDefault();
+		saveGame(gameName, grid);
+	};
 
-    const handleSaveEvent = (e: any) => {
-        e.preventDefault();
-        saveGame(gameName, grid);
-    }
+	return (
+		<StyledContainer background={background}>
+			<GamesContainer>
+				{games.map((game, i) => (
+					<h1 key={i}>
+						<button onClick={() => handleShowSavedGame(game.name)}>
+							{game.name}
+						</button>
+					</h1>
+				))}
+			</GamesContainer>
 
-    console.log(games)
-
-    return (
-        <StyledContainer background={background}>
-            <GamesContainer>
-                {games.map((game, i) =>
-                    <h1 key={i}>
-                        <button onClick={() => handleShowSavedGame(game.gameName)}>
-                            {game.gameName}
-                        </button>
-                    </h1>)}
-            </GamesContainer>
-
-            <TicTacToeContainer>
-                {winner !== null &&
-                    <SaveGameForm>
-                        <label>
-                            NAME YOUR GAME
-                            <input type="text" onChange={(e) => setGameName(e.target.value)} />
-                            <button onClick={(e) => handleSaveEvent(e)}>Save</button>
-                        </label>
-                    </SaveGameForm>
-                    // <h1> THE WINNER IS {winner}</h1>
-                }
-                <StyledGrid>
-                    {
-                        grid.map((row, rowIndex) => (
-                            row.map((cell, colIndex) => {
-                                const key = `${rowIndex}-${colIndex}`;
-                                return <Square cell={cell} key={key} index={key} handleMarker={handleMarker} />
-                            })
-                        ))
-                    }
-                </StyledGrid>
-            </TicTacToeContainer>
-        </StyledContainer>
-    );
-}
+			<TicTacToeContainer>
+				{
+					winner !== null && (
+						<SaveGameForm>
+							<label>
+								NAME YOUR GAME
+								<input
+									type="text"
+									onChange={(e) => setGameName(e.target.value)}
+								/>
+								<button onClick={(e) => handleSaveEvent(e)}>Save</button>
+							</label>
+						</SaveGameForm>
+					)
+					// <h1> THE WINNER IS {winner}</h1>
+				}
+				<StyledGrid>
+					{grid.map((row, rowIndex) =>
+						row.map((cell, colIndex) => {
+							const key = `${rowIndex}-${colIndex}`;
+							return (
+								<Square
+									cell={cell}
+									key={key}
+									index={key}
+									handleMarker={handleMarker}
+								/>
+							);
+						})
+					)}
+				</StyledGrid>
+			</TicTacToeContainer>
+		</StyledContainer>
+	);
+};
 
 type board = string[][];
 
--- web/src/context/GameProvider.tsx
+++ web/src/context/GameProvider.tsx
@@ -1,83 +1,88 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 interface Game {
-    gameName: string;
-    board: string[][];
+	name: string;
+	board: string[][];
 }
 
 type Games = Game[];
 
 interface GameContextType {
-    games: Games;
-    saveGame: (gameName: string, board: string[][]) => void;
-    getGame: (gameName: string) => void;
+	games: Games;
+	saveGame: (gameName: string, board: string[][]) => void;
+	getGame: (gameName: string) => void;
 }
 
 const GameContext = createContext<GameContextType>({
-    games: [],
-    saveGame: () => { },
-    getGame: () => { }
+	games: [],
+	saveGame: () => {},
+	getGame: () => {},
 });
 
-export const GameProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
-    const [games, setGames] = useState<Games>([])
+export const GameProvider: React.FC<React.PropsWithChildren<{}>> = ({
+	children,
+}) => {
+	const [games, setGames] = useState<Games>([]);
 
-    useEffect(() => {
-        fetchGames();
-    }, [])
+	useEffect(() => {
+		fetchGames();
+	}, []);
 
-    const fetchGames = async () => {
-        try {
-            const response = await fetch('http://127.0.0.1:3001/games');
-            if (!response.ok) {
-                throw new Error('OHHH NNOOOOOOO')
-            }
-            const data = await response.json();
-            setGames(data);
-        } catch (error) {
-            console.error("There was something wrong with getting the game data", error)
-        }
-    }
+	const fetchGames = async () => {
+		try {
+			const response = await fetch("http://127.0.0.1:3001/games");
+			if (!response.ok) {
+				throw new Error("OHHH NNOOOOOOO");
+			}
+			const data = await response.json();
+			setGames(data);
+		} catch (error) {
+			console.error(
+				"There was something wrong with getting the game data",
+				error
+			);
+		}
+	};
 
-    const saveGame = async (gameName: string, board: string[][]) => {
-        try {
-            const response = await fetch('http://127.0.0.1:3001/games', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ gameName, board })
-            })
+	const saveGame = async (gameName: string, board: string[][]) => {
+		try {
+			const response = await fetch("http://127.0.0.1:3001/games", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ gameName, board }),
+			});
 
-            if (!response.ok) {
-                throw new Error('OHHHH NOOOOOOO');
-            }
-            const data = await response.json();
-            setGames(data)
-        } catch (error) {
-            console.error("THERE BE SUMTING WRONG WITH DAT", error)
-        }
-    }
+			if (!response.ok) {
+				throw new Error("OHHHH NOOOOOOO");
+			}
+			const data = await response.json();
+			console.log(data);
+			setGames(data);
+		} catch (error) {
+			console.error("THERE BE SUMTING WRONG WITH DAT", error);
+		}
+	};
 
-    const getGame = async (gameName: string) => {
-        try {
-            const response = await fetch(`http://127.0.0.1:3001/game/${gameName}`);
-            if (!response.ok) {
-                throw new Error('OHHH NNOOOOOOO')
-            }
-            const data = await response.json();
-            return data;
-        } catch (e) {
-            console.error("THERE BE SUMTING WRONG WITH DAT", e);
-        }
-    }
+	const getGame = async (gameName: string) => {
+		try {
+			const response = await fetch(`http://127.0.0.1:3001/game/${gameName}`);
+			if (!response.ok) {
+				throw new Error("OHHH NNOOOOOOO");
+			}
+			const data = await response.json();
+			return data;
+		} catch (e) {
+			console.error("THERE BE SUMTING WRONG WITH DAT", e);
+		}
+	};
 
-    return (
-        <GameContext.Provider value={{ games, saveGame, getGame }}>
-            {children}
-        </GameContext.Provider>
-    )
-}
-
+	return (
+		<GameContext.Provider value={{ games, saveGame, getGame }}>
+			{children}
+		</GameContext.Provider>
+	);
+};
 
 export const useGameContext = (): GameContextType => useContext(GameContext);
